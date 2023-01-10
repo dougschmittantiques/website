44cb5f23a3e4d55fcd94173c6b5804ebf005da96
sonofaforester <sonofaforester@gmail.com>
2023-01-09 22:26:26 -0500

    Update update_dns.sh

--- update_dns.sh
+++ update_dns.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-DOMAIN_SERIAL=`curl --get https://server3.iqnecthosting.com:2083/execute/DNS/parse_zone?zone=dougschmittantiques.com -u 'dsaadmin:Website2023!!' | jq '.data[] | select(.record_type == "SOA") | .data_b64[2] | @base64d |tonumber'`
-DOMAIN_INDEX=`curl --get https://server3.iqnecthosting.com:2083/execute/DNS/parse_zone?zone=dougschmittantiques.com -u 'dsaadmin:Website2023!!' | jq '.data[] | select(.dname_b64 == "X2FjbWUtY2hhbGxlbmdl") | .line_index'`
+DOMAIN_SERIAL=`curl --get https://server3.iqnecthosting.com:2083/execute/DNS/parse_zone?zone=dougschmittantiques.com -u 'dsaadmin:$FTP_PASSWORD' | jq '.data[] | select(.record_type == "SOA") | .data_b64[2] | @base64d |tonumber'`
+DOMAIN_INDEX=`curl --get https://server3.iqnecthosting.com:2083/execute/DNS/parse_zone?zone=dougschmittantiques.com -u 'dsaadmin:$FTP_PASSWORD' | jq '.data[] | select(.dname_b64 == "X2FjbWUtY2hhbGxlbmdl") | .line_index'`
 
 echo $DOMAIN_INDEX
 
 echo "edit={'line_index': $DOMAIN_INDEX, 'dname':'_acme-challenge', 'ttl':6400, 'record_type':'TXT', 'data':['$1']}"
 
-curl --get https://server3.iqnecthosting.com:2083/execute/DNS/mass_edit_zone --data "zone=dougschmittantiques.com" --data "serial=$DOMAIN_SERIAL" --data "edit={\"line_index\":$DOMAIN_INDEX,\"dname\":\"_acme-challenge\",\"ttl\":6400,\"record_type\":\"TXT\",\"data\":[\"$1\"]}" -u 'dsaadmin:Website2023!!'
+curl --get https://server3.iqnecthosting.com:2083/execute/DNS/mass_edit_zone --data "zone=dougschmittantiques.com" --data "serial=$DOMAIN_SERIAL" --data "edit={\"line_index\":$DOMAIN_INDEX,\"dname\":\"_acme-challenge\",\"ttl\":6400,\"record_type\":\"TXT\",\"data\":[\"$CERTBOT_VALIDATION\"]}" -u 'dsaadmin:$FTP_PASSWORD'
