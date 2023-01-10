0d4d28c930771ffabeedaead03ab0b554806d08c
Mark Edwards <sonofaforester@gmail.com>
2023-01-10 11:15:32 -0500

    password

--- update-dns.sh
+++ update-dns.sh
@@ -4,7 +4,7 @@ FTP_PASSWORD=`cat /tmp/out1.txt`
 echo $FTP_PASSWORD
 echo "dsaadmin:$FTP_PASSWORD"
 
-DOMAIN_SERIAL=`curl --get https://server3.iqnecthosting.com:2083/execute/DNS/parse_zone?zone=dougschmittantiques.com -u 'dsaadmin:$FTP_PASSWORD' | jq '.data[] | select(.record_type == "SOA") | .data_b64[2] | @base64d |tonumber'`
-DOMAIN_INDEX=`curl --get https://server3.iqnecthosting.com:2083/execute/DNS/parse_zone?zone=dougschmittantiques.com -u 'dsaadmin:$FTP_PASSWORD' | jq '.data[] | select(.dname_b64 == "X2FjbWUtY2hhbGxlbmdl") | .line_index'`
+DOMAIN_SERIAL=`curl --get https://server3.iqnecthosting.com:2083/execute/DNS/parse_zone?zone=dougschmittantiques.com -u "dsaadmin:$FTP_PASSWORD" | jq '.data[] | select(.record_type == "SOA") | .data_b64[2] | @base64d |tonumber'`
+DOMAIN_INDEX=`curl --get https://server3.iqnecthosting.com:2083/execute/DNS/parse_zone?zone=dougschmittantiques.com -u "dsaadmin:$FTP_PASSWORD" | jq '.data[] | select(.dname_b64 == "X2FjbWUtY2hhbGxlbmdl") | .line_index'`
 
-curl --get https://server3.iqnecthosting.com:2083/execute/DNS/mass_edit_zone --data "zone=dougschmittantiques.com" --data "serial=$DOMAIN_SERIAL" --data "edit={\"line_index\":$DOMAIN_INDEX,\"dname\":\"_acme-challenge\",\"ttl\":6400,\"record_type\":\"TXT\",\"data\":[\"$CERTBOT_VALIDATION\"]}" -u 'dsaadmin:$FTP_PASSWORD'
+curl --get https://server3.iqnecthosting.com:2083/execute/DNS/mass_edit_zone -u "dsaadmin:$FTP_PASSWORD" --data "zone=dougschmittantiques.com" --data "serial=$DOMAIN_SERIAL" --data "edit={\"line_index\":$DOMAIN_INDEX,\"dname\":\"_acme-challenge\",\"ttl\":6400,\"record_type\":\"TXT\",\"data\":[\"$CERTBOT_VALIDATION\"]}" 
