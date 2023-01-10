3de6e80c8822c00f37838c5365f8b867bc1ed065
Mark Edwards <sonofaforester@gmail.com>
2023-01-10 16:43:02 -0500

    check curl results

--- update-dns.sh
+++ update-dns.sh
@@ -4,4 +4,15 @@ FTP_PASSWORD=`cat /tmp/out1.txt`
 
 DOMAIN_SERIAL=`curl --get https://server3.iqnecthosting.com:2083/execute/DNS/parse_zone?zone=dougschmittantiques.com -u "dsaadmin:$FTP_PASSWORD" | jq '.data[] | select(.record_type == "SOA") | .data_b64[2] | @base64d |tonumber'`
 
-curl --get https://server3.iqnecthosting.com:2083/execute/DNS/mass_edit_zone -u "dsaadmin:$FTP_PASSWORD" --data "zone=dougschmittantiques.com" --data "serial=$DOMAIN_SERIAL" --data "add={\"dname\":\"_acme-challenge\",\"ttl\":3600,\"record_type\":\"TXT\",\"data\":[\"$CERTBOT_VALIDATION\"]}" 
+RESULT=`curl --get https://server3.iqnecthosting.com:2083/execute/DNS/mass_edit_zone -u "dsaadmin:$FTP_PASSWORD" --data "zone=dougschmittantiques.com" --data "serial=$DOMAIN_SERIAL" --data "add={\"dname\":\"_acme-challenge\",\"ttl\":3600,\"record_type\":\"TXT\",\"data\":[\"$CERTBOT_VALIDATION\"]}"`
+
+echo $RESULT
+
+CURL_STATUS=`echo $RESULT | jq .status`
+
+if [ "$CURL_STATUS" = "1" ]; then
+    exit 0
+else
+    exit 1
+fi
+
