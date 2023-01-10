5915af2b08bfff00305f4a7f62fd6f4725eb9543
Mark Edwards <sonofaforester@gmail.com>
2023-01-09 22:58:00 -0500

    pass

--- update-dns.sh
+++ update-dns.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+echo $FTP_PASSWORD
 echo "dsaadmin:$FTP_PASSWORD"
 
 DOMAIN_SERIAL=`curl --get https://server3.iqnecthosting.com:2083/execute/DNS/parse_zone?zone=dougschmittantiques.com -u "dsaadmin:$FTP_PASSWORD" | jq '.data[] | select(.record_type == "SOA") | .data_b64[2] | @base64d |tonumber'`
